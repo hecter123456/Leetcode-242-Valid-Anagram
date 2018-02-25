ddbd83d709243c9b1bb8f1448b1e96d43d99041d
hecter <hecter654321@gmail.com>
2018-02-25 11:35:40 +0800

    pass testcase: testSample and testFalseSample by using list

--- Valid Anagram.py
+++ Valid Anagram.py
@@ -1,14 +1,36 @@
 import unittest
+from collections import defaultdict
 
 class unitest(unittest.TestCase):
     def testNone(self):
-        Input = None
-        Output = None
-        self.assertEqual(Solution().test(Input),Output)
+        s = ""
+        t = ""
+        Output = True
+        self.assertEqual(Solution().isAnagram(s,t),Output)
+    def testSample(self):
+        s = "anagram"
+        t = "nagaram"
+        Output = True
+        self.assertEqual(Solution().isAnagram(s,t),Output)
+    def testFalseSample(self):
+        s = "rat"
+        t = "car"
+        Output = False
+        self.assertEqual(Solution().isAnagram(s,t),Output)
 
 class Solution():
-    def test(self):
-        return None
+    def isAnagram(self, s, t):
+        List = []
+        for i in range(len(s)):
+            List.append(s[i])
+        for i in range(len(t)):
+            if t[i] in List:
+                List.remove(t[i])
+            else:
+                return False
+        if List == []:
+            return True
+        return False
 
 if __name__ == '__main__':
     unittest.main()
